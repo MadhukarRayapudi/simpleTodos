c66a35598970cb0367b00678b5c54d53969e25fe
MadhukarRayapudi <madhukarrayapudi@gmail.com>
2024-09-11 13:52:33 +0000

    Final

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
 import TodoItem from '../TodoItem'
 import './index.css'
 
@@ -39,33 +40,139 @@ const initialTodosList = [
 
 // Write your code here
 class SimpleTodos extends Component {
-  state = {todoDetailsList: initialTodosList}
+  state = {
+    userInput: '',
+    todosList: initialTodosList,
+  }
 
   deleteTodo = id => {
-    console.log('delete btn clicked')
+    // console.log('delete btn clicked')
     // console.log(this.state)
-    const {todoDetailsList} = this.state
-    const filteredTodoList = todoDetailsList.filter(
-      eachTodo => eachTodo.id !== id,
-    )
+    const {todosList} = this.state
+    const filteredTodoList = todosList.filter(eachTodo => eachTodo.id !== id)
 
-    this.setState({todoDetailsList: filteredTodoList})
+    this.setState({todosList: filteredTodoList})
+  }
+
+  editTodo = id => {
+    const {todosList} = this.state
+
+    const updatedTodoList = todosList.map(eachTodo => {
+      if (eachTodo.id === id) {
+        return {...eachTodo, editON: true}
+      }
+      return eachTodo
+    })
+
+    this.setState({todosList: updatedTodoList})
+  }
+
+  saveTodo = id => {
+    const {todosList} = this.state
+
+    const updatedTodoList = todosList.map(eachTodo => {
+      if (eachTodo.id === id) {
+        return {...eachTodo, editON: false}
+      }
+      return eachTodo
+    })
+    this.setState({todosList: updatedTodoList})
+  }
+
+  onChangeInput = event => {
+    this.setState({userInput: event.target.value})
+  }
+
+  onEditTodo = (id, newTitle) => {
+    const {todosList} = this.state
+
+    const updatedTodoList = todosList.map(eachTodo => {
+      if (eachTodo.id === id) {
+        return {...eachTodo, title: newTitle}
+      }
+      return eachTodo
+    })
+
+    this.setState({todosList: updatedTodoList})
+  }
+
+  onClickAddBtn = () => {
+    const {userInput, todosList} = this.state
+
+    const inputParts = userInput.split(' ')
+    const numberOfTodos = parseInt(inputParts.slice(inputParts.length - 1))
+    // console.log(numberOfTodos)
+
+    let title = userInput
+    let todosToAdd = 1
+
+    if (numberOfTodos > 0) {
+      // Remove the number part from the title
+      title = inputParts.slice(0, inputParts.length - 1).join(' ')
+      console.log(title)
+      todosToAdd = numberOfTodos
+    }
+
+    const newTodos = Array.from({length: todosToAdd}).map(() => ({
+      title,
+      id: uuidv4(),
+      editON: false,
+      todoDone: false,
+    }))
+    this.setState({todosList: [...todosList, ...newTodos], userInput: ''})
+  }
+
+  onClickCheckBox = id => {
+    const {todosList} = this.state
+
+    const updatedTodoList = todosList.map(eachItem => {
+      if (eachItem.id === id) {
+        return {...eachItem, todoDone: !eachItem.todoDone}
+      }
+      return eachItem
+    })
+
+    this.setState({todosList: updatedTodoList})
   }
 
   render() {
-    const {todoDetailsList} = this.state
+    const {todosList, userInput} = this.state
     return (
       <div className="app-bg-container">
         <div className="todos-bg-container">
           <h1 className="todos-heading"> Simple Todos </h1>
-
-          {todoDetailsList.map(eachTodo => (
-            <TodoItem
-              todoDetails={eachTodo}
-              key={eachTodo.id}
-              deleteTodo={this.deleteTodo}
+          <div className="input-and-add-btn-container">
+            <input
+              type="text"
+              id="InputText"
+              placeholder="Enter Here..."
+              className="input-field"
+              onChange={this.onChangeInput}
+              value={userInput}
             />
-          ))}
+            <button
+              className="add-btn"
+              type="button"
+              onClick={this.onClickAddBtn}
+            >
+              Add
+            </button>
+          </div>
+
+          <ul className="todos-ul-container">
+            {todosList.map(eachTodo => (
+              <TodoItem
+                todoDetails={eachTodo}
+                key={eachTodo.id}
+                deleteTodo={this.deleteTodo}
+                editTodo={this.editTodo}
+                editON={eachTodo.editON}
+                saveTodo={this.saveTodo}
+                onEditTodo={this.onEditTodo}
+                onClickCheckBox={this.onClickCheckBox}
+              />
+            ))}
+          </ul>
         </div>
       </div>
     )
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -2,18 +2,63 @@
 import './index.css'
 
 const TodoItem = props => {
-  const {todoDetails, deleteTodo} = props
-  const {title, id} = todoDetails
+  const {
+    todoDetails,
+    deleteTodo,
+    editTodo,
+    saveTodo,
+    onEditTodo,
+    onClickCheckBox,
+  } = props
+  const {title, id, editON, todoDone} = todoDetails
+
+  const textClassName = todoDone ? 'strike-text todos' : 'todos'
   const onDelete = () => {
     deleteTodo(id)
   }
 
+  const onEdit = () => {
+    editTodo(id)
+  }
+
+  const onSave = () => {
+    saveTodo(id)
+  }
+
+  const editTodoTitle = event => {
+    onEditTodo(id, event.target.value)
+  }
+
+  const checkboxClicked = () => {
+    onClickCheckBox(id)
+  }
+
   return (
     <li className="todoItem-btn-container">
-      <p className="todos"> {title} </p>
-      <button className="delete-btn" onClick={onDelete}>
+      <input type="checkbox" className="check-box" onClick={checkboxClicked} />
+      {editON ? (
+        <input
+          type="input"
+          onChange={editTodoTitle}
+          value={title}
+          className="edit-box"
+        />
+      ) : (
+        <p className={textClassName}> {title} </p>
+      )}
+      <button className="delete-btn" onClick={onDelete} type="button">
         Delete
       </button>
+
+      {editON ? (
+        <button className="edit-btn" type="button" onClick={onSave}>
+          Save
+        </button>
+      ) : (
+        <button className="edit-btn" type="button" onClick={onEdit}>
+          Edit
+        </button>
+      )}
     </li>
   )
 }
